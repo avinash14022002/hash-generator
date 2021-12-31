114dc568ff07ec735093466f00c4cadfaab31c3c
Avinash Vijayvargiya <avinash14022002@gmail.com>
2021-12-31 16:35:01 +0530

    Completed: SuccessFragment.kt

--- app/src/main/java/com/example/hashgenerator/SuccessFragment.kt
+++ app/src/main/java/com/example/hashgenerator/SuccessFragment.kt
@@ -1,16 +1,76 @@
 package com.example.hashgenerator
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import com.example.hashgenerator.databinding.FragmentSuccessBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 class SuccessFragment : Fragment() {
+
+    private val args: SuccessFragmentArgs by navArgs()
+
+    private var _binding: FragmentSuccessBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_success, container, false)
+        _binding = FragmentSuccessBinding.inflate(inflater, container, false)
+
+        binding.hashTextView.text = args.hash
+
+        binding.copyButton.setOnClickListener {
+            onCopyClicked()
+        }
+        binding.shareButton.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, "Be Secure With Secure It \n\n\n" + args.hash)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share To:"))
+        }
+        return binding.root
+    }
+
+    private fun onCopyClicked() {
+        lifecycleScope.launch {
+            copyToClipboard(args.hash)
+            applyAnimation()
+        }
+    }
+
+    private fun copyToClipboard(hash: String) {
+        val clipboardManager =
+            requireActivity().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText("Encrypted Text", hash)
+        clipboardManager.setPrimaryClip(clipData)
+
+    }
+
+
+    private suspend fun applyAnimation() {
+        binding.include.messageBackground.animate().translationY(80f).duration = 200L
+        binding.include.messageTextview.animate().translationY(80f).duration = 200L
+
+        delay(2000L)
+
+        binding.include.messageBackground.animate().translationY(-80f).duration = 500L
+        binding.include.messageTextview.animate().translationY(-80f).duration = 500L
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
